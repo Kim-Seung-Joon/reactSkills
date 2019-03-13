20ef146f103fb888e3d380bffb73383183b592cc
Kim-Seung-Joon <wldhel9228@gmail.com>
2019-03-13 13:12:27 +0900

    "컴포넌트 스타일 방법(Sass,Scss)"

---
+++ styling-react/src/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+import "./App.css";
+import SassComponent from './SassComponent'
+
+class App extends Component {
+  render() {
+    return (
+      <SassComponent/>
+    );
+  }
+}
+
+export default App;
