89e733614203e9af79369e8e5e2cec8a105d967e
Kim-Seung-Joon <wldhel9228@gmail.com>
2019-03-18 16:03:43 +0900

    "분리되어 있는 2개의 reducers를 하나의 리듀서로 합쳐주기 combineReducers({})"

--- chap13redux-counter/src/reducers/index.js
+++ chap13redux-counter/src/reducers/index.js
@@ -1,34 +1,11 @@
-import * as types from "../actions/ActionTypes";
+import number from './number'
+import color from './color'
 
-// 초리듀서의 초기상태를 정의한 함수
-const initialState = {
-  color: "black",
-  number: 0
-};
+import {combineReducers} from 'redux';
 
-// 리듀서 함수 정의하기
-// state와 action을 파라미터로 받고 state값이 정의 되어 있지 않으면 initialState를 기본값으로 사용
-// aciton type의 상태는 3개가 있는데 증가,감소, SET_COLOR 3개로 볼 수 있다.
-function counter(state = initialState, action)  {
-  switch(action.type) {
-    case types.INCREMENT:
-    return {
-      ...state,
-      number: state.number+1
-    };
-    case types.DECREMENT:
-    return {
-      ...state,
-      number: state.number+1
-    }
-    case types.SET_COLOR:
-    return {
-      ...state,
-      color: action.color
-    };
-    default:
-      return state;
-  }
-}
+const reducers = combineReducers({
+  numberData: number,
+  colorData: color,
+})
 
-export default counter;
+export default reducers;
