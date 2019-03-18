6c9e87c4739cf04261bb020b552d29737062b4c8
Kim-Seung-Joon <wldhel9228@gmail.com>
2019-03-18 15:23:23 +0900

    "액션 생성"

--- chap13redux-counter/src/actions/ActionTypes.js
+++ chap13redux-counter/src/actions/ActionTypes.js
@@ -2,3 +2,7 @@
    앞에 export를 붙이면 나중에 부를때,
    import *as types from './ActionTypes로 부를 수 있다.
 */ 
+
+export const INCREMENT = 'INCREMENT';
+export const DECREMENT = 'DECREMENT';
+export const SET_COLOR = 'SET_COLOR';
