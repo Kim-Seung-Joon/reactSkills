f42acc58e742800b49cff9c9a1497572e6be24ab
Kim-Seung-Joon <wldhel9228@gmail.com>
2019-03-18 19:04:57 +0900

    "최종적으로 App에 적용하기"

--- chap13redux-counter/src/containers/App.js
+++ chap13redux-counter/src/containers/App.js
@@ -1,17 +1,33 @@
 import React, {Component} from 'react';
-import Counter from '../components/Counter';
+import CounterListContainer from '../containers/CounterListContainer';
+import Buttons from '../components/Buttons';
+import getRandomColor from '../lib/getRandomColor';
 
 
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
 class App extends Component {
   render()  {
+
+    const {onCreate, onRemove} = this.props;
+
     return(
-      <div>
-        <Counter/>
+      <div className="App">
+      <Buttons
+      onCreate={onCreate}
+      onRemove={onRemove}
+      />
+      <CounterListContainer/>
       </div>
     )
   }
 }
 
+const mapToDispatch =(dispatch) =>({
+  onCreate: ()=>dispatch(actions.create(getRandomColor())),
+  onRemove: ()=>dispatch(actions.remove())
+})
 
-export default App;
+
+export default connect(null, mapToDispatch)(App);
